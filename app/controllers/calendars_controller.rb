9a454afc5d3a0a8da06e9663df2b84350b57fd23
Daihuku2 <70841018+Daihuku2@users.noreply.github.com>
2020-09-28 15:16:52 +0900

    Revert "issue6"
    This reverts commit 72abc68f426c8276946e8f5379691b652814d268.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -25,9 +25,6 @@ class CalendarsController < ApplicationController
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
-    # Date.today.wday
-    
-
     @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
@@ -37,7 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date+x).wday]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
